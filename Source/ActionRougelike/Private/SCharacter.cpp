165df3ade8b1c7ae856990bb1eebae8898acfbea
fangjin.wan <fangjin.wan@joyworkshop.com>
2022-08-02 16:42:15 +0800

    Finish Lecture5
    -finish lecture blueprint101

--- Source/ActionRougelike/Private/SCharacter.cpp
+++ Source/ActionRougelike/Private/SCharacter.cpp
@@ -125,6 +125,7 @@ void ASCharacter::PrimaryAttack_TimeElapsed()
 
 	FActorSpawnParameters SpawnParams;
 	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+	SpawnParams.Instigator = this;
 
 	GetWorld()->SpawnActor<AActor>(ProjectileClass, SpawnTM, SpawnParams);
 }
