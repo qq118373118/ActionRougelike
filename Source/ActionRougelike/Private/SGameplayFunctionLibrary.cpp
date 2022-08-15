cf4eb11c7555adb6e38b34dc216757a95c7c73d6
fangjin.wan <fangjin.wan@joyworkshop.com>
2022-08-15 18:08:29 +0800

    fix bug
    - Improving the Physics Impulses on bot ragdolls

--- Source/ActionRougelike/Private/SGameplayFunctionLibrary.cpp
+++ Source/ActionRougelike/Private/SGameplayFunctionLibrary.cpp
@@ -21,7 +21,10 @@ if (ApplyDamage(DamageCauser, TargetActor, DamageAmout))
 		UPrimitiveComponent* HitComp = HitResult.GetComponent();
 		if (HitComp && HitComp->IsSimulatingPhysics(HitResult.BoneName))
 		{
-			HitComp->AddImpulseAtLocation(-HitResult.ImpactNormal * 300000.f, HitResult.ImpactPoint, HitResult.BoneName);
+			FVector Direction = HitResult.TraceEnd - HitResult.TraceStart;
+			Direction.Normalize();
+
+			HitComp->AddImpulseAtLocation(Direction * 300000.f, HitResult.ImpactPoint, HitResult.BoneName);
 		}
 		return true;
 	}
